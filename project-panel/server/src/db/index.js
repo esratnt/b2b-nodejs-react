899799ebfb7182f5d3359f594eae7217c67d539f
esratnt <tontuesra@gmail.com>
2023-12-29 14:58:38 +0300

    database-configurations

--- project-panel/server/src/db/index.js
+++ project-panel/server/src/db/index.js
@@ -1,15 +1,13 @@
-const { text } = require('express')
-const {Pool} = require('pg')
+const { text } = require("express");
+const { Pool } = require("pg");
 const pool = new Pool({
-    user:'postgres',
-    host:'localhost',
-    database:'toptanci',
-    password:'4536',
-    port:5432
-})
-
-
+  user: "",
+  host: "",
+  database: "",
+  password: "",
+  port: 5432,
+});
 
 module.exports = {
-    query: (text , params) => pool.query(text,params),
-}
+  query: (text, params) => pool.query(text, params),
+};
